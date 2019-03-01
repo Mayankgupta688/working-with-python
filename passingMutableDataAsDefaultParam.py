146d8c893ae017d385725bebc28e7f403e72c8eb
Mayank Gupta (ARC) <mayank.gupta@iontrading.com>
2019-03-01 15:33:52 +0530

    Default Mutable Parameters

---
+++ passingMutableDataAsDefaultParam.py
@@ -0,0 +1,21 @@
+# Mutable Data as a parameter can be decieving some time..
+
+# If a parameter is sent to the function, it is sent as reference
+
+# But in case, no parameter is sent, a default data parameter is assigned
+
+# This default parameter is not reassigned every time
+
+def add_user_to_list(user = []):
+    user.append("Mayank")
+    print(user)
+
+userList = ["Anshul", "Mayank"]
+
+add_user_to_list(userList)
+
+add_user_to_list()
+
+add_user_to_list()
+
+# Default Parameter assignment is done onle once..
