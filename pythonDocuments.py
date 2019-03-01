898b67ae3ee8bd23493c33d795deabccf0ec46d0
Mayank Gupta (ARC) <mayank.gupta@iontrading.com>
2019-03-01 12:25:19 +0530

    Added Documentations

---
+++ pythonDocuments.py
@@ -0,0 +1,20 @@
+def sample_documentation(data):
+
+    """ This is Sample Documentation for the Function
+
+    Args:
+        data: Takes a sample input data
+
+    Returns: 
+        Return the same Argument Data
+    """
+
+    print(data)
+
+
+"""
+    Use the following commands to get the documentation:
+
+    1. import pythonDocumentations
+    2. help(pythonDocumentations.sample_documentation)
+"""
