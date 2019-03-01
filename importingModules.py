90684e53f775849fbe25da2c031084e50be0f689
Mayank Gupta (ARC) <mayank.gupta@iontrading.com>
2019-03-01 10:33:09 +0530

    Added Basic Function Definition

---
+++ importingModules.py
@@ -0,0 +1,7 @@
+import pythonBasics
+
+# Working with List Data
+
+employeeNames = ["Mayank", "Anshul", "Meha"]
+
+pythonBasics.show_employee_list(employeeNames)
---
+++ pythonBasics.py
@@ -0,0 +1,12 @@
+# Defining Functions
+
+employeeNames = ["Mayank", "Anshul", "Meha"]
+
+def show_employee_list(emp_list):
+    for employee in emp_list:
+        print(employee)
+
+if __name__ == '__main__':
+    show_employee_list(employeeNames)
+
+
