832247be9f06c3b841fa83949c728c37d46c246f
Mayank Gupta (ARC) <mayank.gupta@iontrading.com>
2019-03-05 10:26:48 +0530

    Working with Tuple Data

--- workingWithTuple.py
+++ workingWithTuple.py
@@ -25,3 +25,23 @@ emptyTuple = ()
 concatTuple = emptyTuple + (1, 2, 3)
 
 print(concatTuple)
+
+# Destructuring the Tuples
+
+(a, (b, (c, d))) = ('1', ('2', ('3', '4')))
+
+print(a + " " + b + " " + c + " " + d)
+
+# Type Conversion in Tupple
+
+myNewTuple = tuple([1, 2, 3, 4])
+
+print(type(myNewTuple))
+
+# Checking Values in Tuple
+
+print(5 in (1, 2, 3, 4))
+
+# Tuple are non mutable type of data
+
+concatTuple[0] = 10
